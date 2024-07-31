5d84cce09f2f8fd6178ade4252dd46c29fe23a5b
dondrakon <dastin77@icloud.com>
2024-07-31 22:16:44 +0300

    Add logging and improve error handling in Camera adapter

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,26 +2,61 @@ package main
 
 import (
 	"log"
+	"time"
+
+	"true/internal/adapters"
 	"true/internal/config"
 
 	"go.uber.org/zap"
 )
 
 func main() {
-
-	cfg, err := config.LoadConfig()
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	// Настройка логгера
 	logger, err := zap.NewDevelopment()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer logger.Sync()
 
-	logger.Info("Config loaded", zap.Any("config", cfg))
+	// Загрузка конфигурации
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		logger.Fatal("Failed to load config", zap.Error(err))
+	}
+	logger.Debug("Loaded config", zap.String("config", cfg.String()))
 
-	
+	// Инициализация адаптера камеры
+	camera := adapters.NewСamera(cfg.Camera, logger)
+	logger.Debug("Initializing camera adapter", zap.String("address", cfg.Camera))
 
+	// Подключение к камере
+	logger.Debug("Connecting to camera", zap.String("address", cfg.Camera))
+	if err := camera.Connect(); err != nil {
+		logger.Fatal("Failed to connect to camera", zap.Error(err))
+	}
+	defer func() {
+		if err := camera.Close(); err != nil {
+			logger.Error("Failed to close camera connection", zap.Error(err))
+		}
+	}()
+
+	logger.Debug("Camera connected", zap.String("address", cfg.Camera))
+
+	// Отправка команды на камеру
+	logger.Debug("Sending command to camera")
+	if err := camera.SendCommand(" "); err != nil {
+		logger.Error("Failed to send command", zap.Error(err))
+	}
+
+	// Чтение ответа от камеры
+	logger.Debug("Reading response from camera")
+	response, err := camera.Read()
+	if err != nil {
+		logger.Error("Failed to read response from camera", zap.Error(err))
+	} else {
+		logger.Info("Received response from camera", zap.String("response", response))
+	}
+
+	// Ожидание для демонстрации работы программы
+	time.Sleep(5 * time.Second)
 }
---
+++ internal/adapters/camera.go
@@ -0,0 +1,105 @@
+package adapters
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+var rwTimeout = 1 * time.Second   // Таймаут для чтения/записи
+var dialTimeout = 5 * time.Second // Таймаут для подключения
+
+type Camera struct {
+	Address string
+	Conn    net.Conn
+	Logger  *zap.Logger
+}
+
+// NewCamera создает новый экземпляр камеры
+func NewСamera(address string, logger *zap.Logger) *Camera {
+	return &Camera{
+		Address: address,
+		Logger:  logger,
+	}
+}
+
+// Connect пытается подключиться к камере
+func (c *Camera) Connect() error {
+	op := fmt.Sprintf("Camera.Connect: %s", c.Address)
+
+	c.Logger.Info("Attempting to connect to camera", zap.String("operation", op))
+
+	conn, err := net.DialTimeout("tcp", c.Address, dialTimeout)
+	if err != nil {
+		c.Logger.Error("Failed to connect to camera", zap.String("operation", op), zap.Error(err))
+		return fmt.Errorf("operation: %s, error: %w", op, err)
+	}
+
+	c.Conn = conn
+	c.Logger.Info("Successfully connected to camera", zap.String("operation", op))
+	return nil
+}
+
+// Close closes the connection to the camera
+func (c *Camera) Close() error {
+	if c.Conn != nil {
+		c.Logger.Info("Closing connection to camera", zap.String("address", c.Address))
+		err := c.Conn.Close()
+		if err != nil {
+			c.Logger.Error("Failed to close connection to camera", zap.String("address", c.Address), zap.Error(err))
+			return err
+		}
+		c.Logger.Info("Successfully closed connection to camera", zap.String("address", c.Address))
+	}
+	return nil
+}
+
+// SendCommand sends a command to the camera
+func (c *Camera) SendCommand(cmd string) error {
+	if c.Conn == nil {
+		err := fmt.Errorf("no active connection")
+		c.Logger.Error("Failed to send command", zap.String("command", cmd), zap.Error(err))
+		return err
+	}
+
+	cmd = cmd + "\n"
+	_, err := c.Conn.Write([]byte(cmd))
+	if err != nil {
+		c.Logger.Error("Failed to send command", zap.String("command", cmd), zap.Error(err))
+		return fmt.Errorf("failed to send command '%s': %w", cmd, err)
+	}
+
+	c.Logger.Info("Command sent successfully", zap.String("command", cmd))
+	return nil
+}
+
+// Read reads data from the camera connection
+func (c *Camera) Read() (string, error) {
+	if c.Conn == nil {
+		err := fmt.Errorf("no active connection")
+		c.Logger.Error("Failed to read data", zap.Error(err))
+		return "", err
+	}
+
+	scanner := bufio.NewScanner(c.Conn)
+	c.Conn.SetReadDeadline(time.Now().Add(rwTimeout))
+
+	if scanner.Scan() {
+		text := scanner.Text()
+		c.Logger.Info("Data read from camera", zap.String("data", text))
+		return text, nil
+
+	}
+
+	err := scanner.Err()
+	if err != nil {
+		c.Logger.Error("Failed to read data", zap.Error(err))
+		return "", err
+	}
+
+	c.Logger.Info("No data read from camera")
+	return "", nil
+}
